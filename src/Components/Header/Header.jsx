f19b7cf6039ba99431de5f092a88b59c7c42464d
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-12-30 12:12:09 +0330

    add header section

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,29 @@
+const Header = () => {
+    return(
+        <div className="flex items-center gap-x-2">
+            <div className="relative">
+                <div className="absolute">
+
+                </div>
+            </div>
+            <div className="flex items-center gap-x-2">
+                <a>
+                    درباره ما
+                </a>
+                <a>
+                    گالری تصاویر
+                </a>
+                <a>
+                    خدمات
+                </a>
+                <a>
+                    خدمات برگزیده
+                </a>
+                <a>
+                    همراهان ما
+                </a>
+            </div>
+        </div>
+    )
+}
+export default Header ; 
