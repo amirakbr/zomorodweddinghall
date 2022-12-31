f3d44a0248e19fd3b81dda00cd4fccb54e80e811
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-12-31 09:13:54 +0330

    add customer feed backs

---
+++ src/Components/FeedBack/FeedBack.jsx
@@ -0,0 +1,74 @@
+const FeedBack = () => {
+    return(
+        <div className="flex flex-col items-center container mx-auto">
+            <h4 className="text-2xl">
+                نظرات دیگران درباره قصر زمرد
+            </h4>
+            <div className="flex flex-col">
+                <div className="grid grid-cols-[1fr_1fr_1fr] gap-8">
+                    <div className="
+                    flex flex-col p-2">
+                        <div className="flex items-center gap-4 border-b p-2">
+                            <img src="./Assets/emrlad/OurCustomer/user1.jpg" className="w-[4rem] h-[4rem] rounded-full object-cover object-center"/>
+                            <div>
+                                <p>
+                                    بانک ملت
+                                </p>
+                            </div>
+                        </div>
+                        <article className="text-justify text-lg">
+                            قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما میباشد .  
+                        </article>
+                    </div>
+                    <div className="
+                    flex flex-col p-2">
+                        <div className="flex items-center gap-4 border-b p-2">
+                            <img src="./Assets/emrlad/OurCustomer/user1.jpg" className="w-[4rem] h-[4rem] rounded-full object-cover object-center"/>
+                            <div>
+                                <p>
+                                    بانک ملت
+                                </p>
+                            </div>
+                        </div>
+                        <article className="text-justify text-lg">
+                            قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما میباشد .  
+                        </article>
+                    </div>
+                    <div className="
+                    flex flex-col p-2">
+                        <div className="flex items-center gap-4 border-b p-2">
+                            <img src="./Assets/emrlad/OurCustomer/user2.jpg" className="w-[4rem] h-[4rem] rounded-full object-cover object-center"/>
+                            <div>
+                                <p>
+                                    بانک ملت
+                                </p>
+                            </div>
+                        </div>
+                        <article className="text-justify text-lg">
+                            قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما . قصر مجلل زمرد با بهره گیری از کادری مجرب
+                            بهترین انتخاب برای مجالس شما میباشد .  
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FeedBack ;
