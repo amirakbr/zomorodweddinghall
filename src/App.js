2580ee06a1f91b6448430a1fa451f5efb6a7ce88
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-12-31 09:15:55 +0330

    import all section to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Header from "./Components/Header/Header";
-
-
+import Hero from "./Components/Hero/Hero";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import OurServices from "./Components/OurServices/OurServices";
+import OurCustomer from "./Components/OurCustomer/OurCustomer";
+import Ariticles from "./Components/Ariticles/Ariticles";
+import Gallary from "./Components/Gallary/Gallary";
+import FeedBack from "./Components/FeedBack/FeedBack";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App" dir="rtl">
       <Header />
+      <Hero />
+      <AboutUs />
+      <OurServices />
+      <OurCustomer />
+      <Ariticles />
+      <Gallary />
+      <FeedBack />
+      <Footer />
     </div>
   );
 }
